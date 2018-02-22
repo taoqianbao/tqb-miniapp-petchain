15206705c2af19eb421fa422438ded3832305ecb
zhushengfeng1 <zhushengfeng1@wanda.cn>
2018-02-22 15:04:14 +0800

    client - test helpers

--- client/app.js
+++ client/app.js
@@ -2,11 +2,17 @@
 var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
 
+const helper = require('./utils/helpers')
+
 App({
   onLaunch: function () {
     qcloud.setLoginUrl(config.service.loginUrl)
   },
   onShow: function (options) {
+
+    console.log(helper.formatTime('ddd'))
+    console.log(new helper().formatDate('time'))
+
     console.log(JSON.stringify(options))
   },
   onHide: function () {
---
+++ client/utils/helpers.js
@@ -0,0 +1,23 @@
+/**
+ * Class 
+ */
+class helpers {
+  constructor() {
+
+  }
+  /**
+   * formatDate
+   */
+  formatDate() {
+    return "date"
+  }
+
+  /**
+   * formatTime
+   * param time 
+   */
+  static formatTime(time) {
+    return "time"
+  }
+}
+module.exports = helpers
